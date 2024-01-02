d872299b4c8ac075eaf83a0b406ff0d12f18d861
ameliachen <meifeiccc@gmail.com>
2024-01-02 16:25:42 +0800

    Modified: the monitored CPU time when execFile

--- worker/utils/execFile.js
+++ worker/utils/execFile.js
@@ -11,8 +11,9 @@ export async function execFile(language, filepath, index, input, timeLimit) {
   return new Promise((resolve, reject) => {
     const volumePath = '/app'
 
-    const command = `echo "${input}" | docker run -i --rm --name ${tempFileName}${containerName}${index} -v ${tempFileDir}:${volumePath} ${imageName}:latest timeout ${timeLimit / 1000} /usr/bin/time -f '%e %M' ${runtimeCommand} ${volumePath}/${tempFileName}`
+    const command = `echo "${input}" | docker run -i --rm --name ${tempFileName}${containerName}${index} -v ${tempFileDir}:${volumePath} ${imageName}:latest timeout ${timeLimit / 1000} /usr/bin/time -f '%U %M' ${runtimeCommand} ${volumePath}/${tempFileName}`
     exec(command, (error, stdout, stderr) => {
+      console.log(stderr)
       if (error) {
         if (error.code === 124 || error.code === 2) return reject(new TimeLimitExceededError('Time Limit Exceeded'))
         return reject(new RunTimeError(stderr, error))
